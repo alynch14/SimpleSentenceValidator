c6cce25f0533915a3ca9f2f76b783c6affed1158
Adam Lynch <alynch14@students.towson.edu>
2018-04-18 23:05:03 -0400

    Initial commit

---
+++ SentenceValidator/SentenceValidator/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+let noun str = match str with
+    |"dog" -> true
+    |"cat" -> true
+    |"mouse" -> true
+    |"tree" -> true
+    |"cheese" -> true
+    |_ -> false
+
+let verb str = match str with
+    |"likes" -> true
+    |"eats" -> true
+    |"chases" -> true
+    |_ -> false
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
+
