0e722943a3aaf145b7ea78da3fc9e58524e9c986
Adam Lynch <alynch14@students.towson.edu>
2018-04-25 21:10:36 -0400

    Added argument processor

--- SentenceValidator/SentenceValidator/Program.fs
+++ SentenceValidator/SentenceValidator/Program.fs
@@ -1,4 +1,6 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+
+// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
 let noun str = match str with
@@ -15,9 +17,21 @@ let verb str = match str with
     |"chases" -> true
     |_ -> false
 
+let rec stringProcessor (str:string) (index:int) =
+    if str.[index] = ' ' then index
+    else (stringProcessor str (index+1))
+    
+let rec processList lst index = 
+    let output = match lst with
+        |x::xs -> 
+            (index, (stringProcessor x 0))
+    output
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let argList = 
+        List.ofArray argv
+    let tupleList = 
+        processList argList 0
     0 // return an integer exit code
 
