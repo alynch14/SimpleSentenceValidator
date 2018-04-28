1689ced2faa4cd24f8777f6878930e1bfc23370b
Adam Lynch <alynch14@students.towson.edu>
2018-04-28 16:50:27 -0400

    Added functions to help parse sentences entered by user. Added code into main() to take in input from the user to be parsed.

--- SentenceValidator/SentenceValidator/Program.fs
+++ SentenceValidator/SentenceValidator/Program.fs
@@ -1,7 +1,10 @@
 ﻿open System
+open System.Security.Cryptography.X509Certificates
 
 // Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+
+let mutable errorMessage = " "
 
 let noun str = match str with
     |"dog" -> true
@@ -16,22 +19,81 @@ let verb str = match str with
     |"eats" -> true
     |"chases" -> true
     |_ -> false
-
-let rec stringProcessor (str:string) (index:int) =
-    if str.[index] = ' ' then index
-    else (stringProcessor str (index+1))
     
-let rec processList lst index = 
-    let output = match lst with
-        |x::xs -> 
-            (index, (stringProcessor x 0))
-    output
+let adjective str = match str with
+    |"fast" -> true
+    |"slow" -> true
+    |"furry" -> true
+    |"yellow" -> true
+    |_ -> false
+    
+let adverb str = match str with
+    |"quickly" -> true
+    |"slowly" -> true
+    |"quietly" -> true
+    |_ -> false
 
-[<EntryPoint>]
-let main argv = 
-    let argList = 
-        List.ofArray argv
-    let tupleList = 
-        processList argList 0
-    0 // return an integer exit code
+let article str = match str with
+    |"a" -> true
+    |"the" -> true
+    |_ -> false
+
+let conjunction str = match str with
+    |"and" -> true
+    |"or" -> true
+    |_ -> false
+
+let preposition str = match str with
+    |"of" -> true
+    |"around" -> true
+    |_ -> false
+
+let eos str = match str with
+    |"." -> true
+    |"!" -> true
+    |_ -> false
+
+let comma str = match str with
+    |"," -> true
+    |_ -> false
+
+let rec processNounPhrase lst = 
+    if noun (List.head lst) then true
+    elif List.isEmpty(List.tail lst) then 
+        errorMessage <- "You did not enter a noun into one of your noun phrases."
+        false
+    else processNounPhrase (List.tail lst)
+
+
+
+let rec adjList lst =
+    if not(adjective (List.head lst)) then 
+        errorMessage <- "Invalid word in adjective list." 
+        false
+    elif List.isEmpty(List.tail lst) then adjList (List.tail lst)
+    else true
+
+let rec nounPhrase lst = 
+    if noun (List.head lst) then true
+    else 
+        let index = List.findIndex(List.contains(processNounPhrase lst ))
+        
+
+    
+let rec processString lst =
+    if verb (List.head lst) then List.head lst
+    else processString (List.tail lst)
+
+let stringProcessor lst = 
+    let indexToBeSplitAt = 
+        List.findIndex(List.contains(processString lst))
+    indexToBeSplitAt
+    
+let main () = 
+    Console.Write("Please enter a sentence to be processed: ")
+    let sentence = Console.ReadLine()
+    let list = List.ofArray(sentence.Split ' ')
+    stringProcessor list
+
+
 
